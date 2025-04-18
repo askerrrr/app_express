fa367d1d54fc4eeeefe4a04ee8b666c43534cef9
Asker Sultanov <asker_sultanov1999@mail.ru>
2025-04-18 08:33:06 +0300

    add try catch in index.js

--- src/public/js/index.js
+++ src/public/js/index.js
@@ -1,17 +1,21 @@
 import rowForUserList from "./services/row/rowForUserList.js";
 
 var GetUsers = async () => {
-  var response = await fetch("/api/users");
+  try {
+    var response = await fetch("/api/users");
 
-  if (!response.ok) {
-    return;
-  }
+    if (!response.ok) {
+      return;
+    }
 
-  var users = await response.json();
+    var users = await response.json();
 
-  for (var user of users) {
-    await rowForUserList(user);
+    for (var user of users) {
+      await rowForUserList(user);
+    }
+  } catch (err) {
+    alert("err: ", err);
   }
 };
 
-GetUsers().catch((err) => alert("error: ", err));
+GetUsers();
