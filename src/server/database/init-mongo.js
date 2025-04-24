e6434e3421713d5da1b07d33f0390bb402117ecb
Asker Sultanov <asker_sultanov1999@mail.ru>
2025-04-24 15:31:16 +0300

    update init-mongo.js

--- src/server/database/init-mongo.js
+++ src/server/database/init-mongo.js
@@ -1,17 +1,8 @@
-db.getSiblingDB("admin").auth(
-  process.env.MONGO_INITDB_ROOT_USERNAME,
-  process.env.MONGO_INITDB_ROOT_PASSWORD
-);
+db = db.getSiblingDB("admin");
 
-db.getSiblingDB("admin").createUser({
-  user: process.env.MONGO_ROOT_USER,
-  pwd: process.env.MONGO_ROOT_PASSWD,
-  roles: ["userAdminAnyDatabase", "readWriteAnyDatabase"],
-});
+db.createCollection("admins");
 
-db.getSiblingDB("admin").createCollection("adminData");
-
-db.getSiblingDB("admin").adminData.insertOne({
+db.admins.insertOne({
   login: process.env.HASH_LOGIN,
   passwd: process.env.HASH_PASSWD,
 });
