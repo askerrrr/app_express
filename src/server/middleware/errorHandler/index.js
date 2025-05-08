bf6336743c39cfabed0d2c766721634ca62b7e27
asker <askersultanov666@gmail.com>
2025-05-08 22:15:35 +0300

    handle boterror in errorHandler

--- src/server/middleware/errorHandler/index.js
+++ src/server/middleware/errorHandler/index.js
@@ -1,9 +1,25 @@
 import logger from "../../logger.js";
+import {
+  BotServerError,
+  BotUserCreateError,
+  BotOrderCreateError,
+  BotOrderDetailsError,
+} from "../../customError/index.js";
 
 var errorHandler = async (e, req, res, next) => {
   logger.error({ error: e });
 
-  return res.status(e.status ?? 500).json({ msg: e.message });
+  if (
+    e instanceof BotServerError ||
+    e instanceof BotUserCreateError ||
+    e instanceof BotOrderCreateError ||
+    e instanceof BotOrderDetailsError
+  ) {
+    return res.sendStatus(500);
+  }
+
+  res.status(500);
+  return res.sendFile(join(__dirname, "../../../public/html/errorPage.html"));
 };
 
 export default errorHandler;
