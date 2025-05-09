e79ac13f8d054cc762e2372e55b32d74afbd421f
asker <askersultanov666@gmail.com>
2025-05-09 08:52:31 +0300

    refactoring getOrders, add UserNotFoundError

--- src/server/database/user.collection/services/getOrders.js
+++ src/server/database/user.collection/services/getOrders.js
@@ -1,11 +1,22 @@
-import { DatabaseError } from "../../../customError/index.js";
+import {
+  DatabaseError,
+  UserNotFoundError,
+} from "../../../customError/index.js";
 
 var getOrders = async (collection, userId) => {
   try {
-    var { orders } = await collection.findOne({ userId }).exec();
+    var user = await collection.findOne({ userId }).exec();
 
-    return orders;
+    if (!user) {
+      throw new UserNotFoundError();
+    }
+
+    return user.orders;
   } catch (e) {
+    if (e instanceof UserNotFoundError) {
+      throw e;
+    }
+
     throw new DatabaseError("getOrders", e, userId);
   }
 };
