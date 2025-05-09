fe1c428c26c5fc7da0448c9ebd72762befb9954a
asker <askersultanov666@gmail.com>
2025-05-09 22:10:36 +0300

    refactoring checkPurchasedStatus

--- src/server/router/orderStatus/controllers/checkPurchasedStatus.js
+++ src/server/router/orderStatus/controllers/checkPurchasedStatus.js
@@ -2,9 +2,9 @@ var checkPurchasedStatus = async (req, res, next) => {
   try {
     var { userId, orderId } = req.params;
 
-    var itemCollection = req.app.locals.itemCollectionServices();
+    var { getItemsData } = req.app.locals.itemCollectionServices();
 
-    var { items } = await itemCollection.getItemsData(userId, orderId);
+    var { items } = await getItemsData(userId, orderId);
 
     var isAllItemsIsPurchased = items.every((item) => item.purchased == 1);
 
