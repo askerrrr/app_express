b0b1b1d0aa9559dbbe263a038663eec084eba1ee
asker <askersultanov666@gmail.com>
2025-05-09 09:08:35 +0300

    refactoring getOrderData

--- src/server/router/order/controllers/getOrderData.js
+++ src/server/router/order/controllers/getOrderData.js
@@ -4,9 +4,7 @@ var getOrderData = async (req, res, next) => {
   var userCollection = req.app.locals.userCollectionServices();
 
   try {
-    var user = await userCollection.getUserById(userId);
-
-    var order = user.orders.find((order) => order.id === orderId);
+    var order = await userCollection.getOrder(userId, orderId);
 
     return order ? res.json(order) : res.sendStatus(404);
   } catch (e) {
