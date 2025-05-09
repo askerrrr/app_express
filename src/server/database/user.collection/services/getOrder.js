c335a7d9100452ffca2b920dbeacc845924c121d
asker <askersultanov666@gmail.com>
2025-05-09 09:12:37 +0300

    handle err in getOrder

--- src/server/database/user.collection/services/getOrder.js
+++ src/server/database/user.collection/services/getOrder.js
@@ -1,13 +1,31 @@
-import { DatabaseError } from "../../../customError/index.js";
+import {
+  DatabaseError,
+  OrderNotFoundError,
+  UserNotFoundError,
+} from "../../../customError/index.js";
 
 var getOrder = async (collection, userId, orderId) => {
   try {
-    var { orders } = await collection.findOne({ userId }).exec();
+    var user = await collection.findOne({ userId }).exec();
+
+    if (!user) {
+      throw new UserNotFoundError();
+    }
+
+    var { orders } = user;
 
     var order = orders.find((order) => order.id == orderId);
 
+    if (!order) {
+      throw new OrderNotFoundError();
+    }
+
     return order;
   } catch (e) {
+    if (e instanceof UserNotFoundError || e instanceof OrderNotFoundError) {
+      throw e;
+    }
+
     throw new DatabaseError("getOrder", e, userId, orderId);
   }
 };
